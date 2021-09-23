dfc048e6164e57ba046943562e1381a555ac75f2
Isaac Mina <isaacmina93@gmail.com>
2021-09-23 13:33:55 -0700

    Create Boot MVC Project

---
+++ KitchnTabl/src/main/java/com/skilldistillery/kitchntabl/KitchnTablApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.kitchntabl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchnTablApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchnTablApplication.class, args);
+	}
+
+}
---
+++ KitchnTabl/src/test/java/com/skilldistillery/kitchntabl/KitchnTablApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.kitchntabl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchnTablApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

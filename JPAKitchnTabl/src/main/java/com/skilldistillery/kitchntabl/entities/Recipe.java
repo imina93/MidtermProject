35900dcd5129f66440ac8920500c9f7c19f3e818
amandue10 <50896687+amandue10@users.noreply.github.com>
2021-09-24 15:07:02 -0600

    ingredient and recipe entity

---
+++ JPAKitchnTabl/src/main/java/com/skilldistillery/kitchntabl/entities/Recipe.java
@@ -0,0 +1,186 @@
+package com.skilldistillery.kitchntabl.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Recipe {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	private String instructions;
+	private int calories;
+	
+	@Column(name="chef_rating")
+	private int chefRating;
+	
+	private String category;
+	
+	@Column(name="serving_size")
+	private String servingSize;
+	
+	@Column(name="cook_time")
+	private int cookTime;
+	
+	@Column(name="user_id")
+	private int userId;
+	
+	private String image;
+	private String description;
+	
+	
+	public Recipe() {
+		super();
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getInstructions() {
+		return instructions;
+	}
+
+
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}
+
+
+	public int getCalories() {
+		return calories;
+	}
+
+
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+
+
+	public int getChefRating() {
+		return chefRating;
+	}
+
+
+	public void setChefRating(int chefRating) {
+		this.chefRating = chefRating;
+	}
+
+
+	public String getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
+	public String getServingSize() {
+		return servingSize;
+	}
+
+
+	public void setServingSize(String servingSize) {
+		this.servingSize = servingSize;
+	}
+
+
+	public int getCookTime() {
+		return cookTime;
+	}
+
+
+	public void setCookTime(int cookTime) {
+		this.cookTime = cookTime;
+	}
+
+
+	public int getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+
+	public String getImage() {
+		return image;
+	}
+
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Recipe other = (Recipe) obj;
+		return id == other.id;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Recipe [id=" + id + ", name=" + name + ", instructions=" + instructions + ", calories=" + calories
+				+ ", chefRating=" + chefRating + ", category=" + category + ", servingSize=" + servingSize
+				+ ", cookTime=" + cookTime + ", userId=" + userId + ", image=" + image + ", description=" + description
+				+ "]";
+	}
+	
+	
+	
+	
+
+}

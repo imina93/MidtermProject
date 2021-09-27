9cd91094c63df2bba41c00575adc40fd64093c65
Me <johnpuliatch@gmail.com>
2021-09-27 13:50:02 -1000

    Working create user on home page commented out a few things mentioned in slack

--- KitchnTabl/src/main/java/com/skilldistillery/kitchntabl/data/UserDAOImpl.java
+++ KitchnTabl/src/main/java/com/skilldistillery/kitchntabl/data/UserDAOImpl.java
@@ -16,6 +16,42 @@ public class UserDAOImpl implements UserDAO {
 	private EntityManager em;
 
 	@Override
+	public User createUser(User user) {
+		em.persist(user);
+		em.flush();
+		em.close();
+		return user;
+	}
+
+	@Override
+	public User updateUser(int id, User user) {
+		User dbUser = em.find(User.class, id);
+		dbUser.setEmail(user.getEmail());
+		dbUser.setUsername(user.getUsername());
+		dbUser.setPassword(user.getPassword());
+		dbUser.setImageURL(user.getImageURL());
+		dbUser.setFirstName(user.getFirstName());
+		dbUser.setLastName(user.getLastName());
+		dbUser.setBiography(user.getBiography());
+
+		em.flush();
+		em.close();
+		return dbUser;
+	}
+
+	@Override
+	public boolean destroyUser(int id) {
+		boolean successfullyRemovedUser = false;
+		User user = em.find(User.class, id);
+		if (user != null) {
+			em.remove(user); // performs the delete on the managed entity
+			successfullyRemovedUser = !em.contains(user);
+		}
+		em.close();
+		return successfullyRemovedUser;
+	}
+
+	@Override
 	public User findByUsername(String username) {
 		String jpql = "SELECT u FROM User u WHERE u.username = :uname";
 		User user = null;

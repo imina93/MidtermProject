cbbdebd92a516eb9d7e92f740defc8135a507899
lohidenver <broadcastdave@gmail.com>
2021-09-27 17:57:58 -0600

    testing branch

--- KitchnTabl/src/main/java/com/skilldistillery/kitchntabl/controllers/LoginController.java
+++ KitchnTabl/src/main/java/com/skilldistillery/kitchntabl/controllers/LoginController.java
@@ -1,5 +1,17 @@
 package com.skilldistillery.kitchntabl.controllers;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class LoginController {
 
+	
+	
+//	@RequestMapping(path = "login.do")
+//	public String home(Model model) {
+//		model.addAttribute("resellers", dao.findAll());
+//		return "home";
+//	}
+	
+	
 }

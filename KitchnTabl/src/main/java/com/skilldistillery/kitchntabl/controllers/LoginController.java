e390142bc538ac0c9c6c1c8fe364e0e7393e749f
lohidenver <broadcastdave@gmail.com>
2021-09-28 10:11:14 -0600

    get current

---
+++ KitchnTabl/src/main/java/com/skilldistillery/kitchntabl/controllers/LoginController.java
@@ -0,0 +1,43 @@
+package com.skilldistillery.kitchntabl.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.skilldistillery.kitchntabl.data.UserDAO;
+import com.skilldistillery.kitchntabl.entities.User;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	private UserDAO dao;
+	
+	@RequestMapping(path = "login.do", method=RequestMethod.GET)
+	public String goToLoginForm() {
+		
+		return "login";
+	}
+	
+	
+	
+	@RequestMapping(path = "login.do", method=RequestMethod.POST)
+	public String logIn(HttpSession session, User user) {
+		user = dao.logUserIn(user);
+		if(user != null) {
+			session.setAttribute("loggedInUser", user);
+			return "user/userPage";
+		}
+		else {
+			return "login";
+		}
+	}
+	@RequestMapping(path = "logout.do")
+	public String logOut(HttpSession session) {
+		session.removeAttribute("loggedInUser");
+		return "login";
+	}
+}
